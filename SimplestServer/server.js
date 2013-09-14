5b56c649cc4eba2f7efb63bf259a4c6d7877265c
Kevin G <zil.kucinskas@gmail.com>
2013-09-14 22:54:19 +0300

    Make request handlers return appropriate content

--- SimplestServer/server.js
+++ SimplestServer/server.js
@@ -6,10 +6,9 @@ function start(route, handle) {
         var pathname = url.parse(request.url).pathname;
         console.log("Request for " + pathname + " received.");
 
-        route(handle, pathname);
-
         response.writeHead(200, {"Content-type" : "text/plain"});
-        response.write("Hello world");
+        var content route(handle, pathname);
+        response.write(content);
         response.end();
     }
 
