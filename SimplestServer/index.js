2ea96fe01745b045be2d1c73a3ed6801aa450d94
Kevin G <zil.kucinskas@gmail.com>
2013-09-14 23:56:31 +0300

    Add simple form

--- SimplestServer/index.js
+++ SimplestServer/index.js
@@ -6,5 +6,6 @@ var handle = {};
 handle["/"] = requestHandlers.start;
 handle["/start"] = requestHandlers.start;
 handle["/upload"] = requestHandlers.upload;
+handle["/list"] = requestHandlers.list;
 
 server.start(router.route, handle);
